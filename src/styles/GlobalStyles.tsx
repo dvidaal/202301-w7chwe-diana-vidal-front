fceb9c4e8a5c3dddb2b023c792744723eb17ce13
Diana Vidal <dvidaal@gmail.com>
2023-02-26 12:20:18 +0100

    Create global styles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+* {
+box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+  monospace;
+  padding: 30px 10px;
+  min-height:100vh;
+}
+
+ul,
+ol,
+li {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  
+}
+
+button {
+  font-family: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 50px;
+  border-radius: 20px;
+  cursor: pointer;
+  border: none
+}
+
+
+h1,
+h2 {
+  padding: 20px;
+  font-size: 30px;
+  margin: 0;
+}
+
+`;
+
+export default GlobalStyles;
